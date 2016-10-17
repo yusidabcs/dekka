022b8d2a3d63fac093775e47f199e54083b04785
agusyusida@gmail <agusyusida@gmail.com>
2016-10-17 14:43:05 +0800

    add google analytic

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -13,8 +13,6 @@
 <script src="js/jquery.min.js"></script>
 <script src="js/scripts.js?v=1.7"></script>
 <!-- //js -->
-<!-- start-smoth-scrolling -->
-<script type="text/javascript" src="js/move-top.js"></script>
 <script type="text/javascript" src="js/easing.js"></script>
 <script type="text/javascript">
 	jQuery(document).ready(function($) {
@@ -130,5 +128,15 @@
 								</script>
 									<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
 	<!-- content-Get-in-touch -->
+	<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-85341349-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 	</body>
 </html>
